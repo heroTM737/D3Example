d82e62fdbc8d9b21f1f5321a993cdc657ab0c520
heroTM737 <xuantien.nguyen93@gmail.com>
2016-02-01 14:50:22 +0700

    update event graph
    update event graph

--- D3 Concept map/eventgraph/eventgraph.js
+++ D3 Concept map/eventgraph/eventgraph.js
@@ -1,105 +1,102 @@
+$.ajax({
+    type: "GET",
+    url: "data/EventGraphData.json",
+    dataType: "text",
+    success: function (data, textStatus, jqXHR) {
+        var result = JSON.parse(data);
+        
+        var circleList = [];
+        var rectList = [];
+        var links = [];
+        
+        for (var i in result.nodes){
+//            console.log(result.nodes[i].type);
+            if (result.nodes[i].type == "event") {
+                circleList.push({
+                    x: 300,
+                    y: 50 + circleList.length * 15,
+                    data: result.nodes[i]
+                });
+                result.nodes[i].cindex = circleList.length - 1;
+            } else if (result.nodes[i].type == "source_target") {
+                rectList.push({
+                    x: 50 + (rectList.length % 2) * 500,
+                    y: 100 + rectList.length * 200,
+                    data: result.nodes[i]
+                });
+                result.nodes[i].cindex = rectList.length - 1;
+            } 
+        }
+        
+        for (var i in result.links) {
+            var source = result.links[i].source;
+            var target = result.links[i].target;
+            var link = {
+                source: {
+                    x: 0,
+                    y: 0
+                },
+                target: {
+                    x: 0,
+                    y: 0
+                }
+            };
+            
+            if (result.nodes[source].type == "event") {
+                link.source.x = circleList[result.nodes[source].cindex].x;
+                link.source.y = circleList[result.nodes[source].cindex].y;
+            } else if (result.nodes[source].type == "source_target") {
+                link.source.x = rectList[result.nodes[source].cindex].x;
+                link.source.y = rectList[result.nodes[source].cindex].y;
+            }
+            
+            if (result.nodes[target].type == "event") {
+                link.target.x = circleList[result.nodes[target].cindex].x;
+                link.target.y = circleList[result.nodes[target].cindex].y;
+            } else if (result.nodes[target].type == "source_target") {
+                link.target.x = rectList[result.nodes[target].cindex].x;
+                link.target.y = rectList[result.nodes[target].cindex].y;
+            }
+            
+            links.push(link);
+        }
+        
+        visualizeData(circleList,  rectList, links);
+    }
+});
+
 //$.ajax({
 //    type: "GET",
-//    url: "data/ExampleData.txt",
+//    url: "data/ExampleData.json",
 //    dataType: "text",
 //    success: function (data, textStatus, jqXHR) {
 //        var result = JSON.parse(data);
-//        for (var i in result.nodes){
-//            console.log(result.nodes[i].type);    
+//        
+//        var circleList = result.circleList;
+//        var rectList = result.rectList;
+//        var links = [];
+//        for (var i = 0; i < circleList.length; i++) {
+//            for (var j = 0; j < rectList.length; j++) {
+//                links.push({
+//                    source: {
+//                        x: circleList[i].x,
+//                        y: circleList[i].y
+//                    },
+//                    target: {
+//                        x: rectList[j].x,
+//                        y: rectList[j].y
+//                    }
+//                });
+//            }
 //        }
+//        
+//        visualizeData(circleList, rectList, links);
 //    }
 //});
 
-var circleList = [
-    {
-        x: 300,
-        y: 50
-    }, {
-        x: 300,
-        y: 100
-    }, {
-        x: 300,
-        y: 150
-    }, {
-        x: 300,
-        y: 200
-    }, {
-        x: 300,
-        y: 250
-    }, {
-        x: 300,
-        y: 300
-    }, {
-        x: 300,
-        y: 350
-    }, {
-        x: 300,
-        y: 400
-    }, {
-        x: 300,
-        y: 450
-    }, {
-        x: 50,
-        y: 50
-    }
-];
-
-var rectList = [
-    {
-        x: 50,
-        y: 50
-    }, {
-        x: 550,
-        y: 150
-    }
-];
-
-var links = [];
-for (var i = 0; i < circleList.length; i++) {
-    for (var j = 0; j < rectList.length; j++) {
-        links.push({
-            source: {
-                x: circleList[i].x,
-                y: circleList[i].y
-            },
-            target: {
-                x: rectList[j].x,
-                y: rectList[j].y
-            }
-        });
-        
-        links.push({
-            source: {
-                x: rectList[j].x,
-                y: rectList[j].y
-            },
-            target: {
-                x: circleList[i].x,
-                y: circleList[i].y
-            }
-        });
-    }
-}
-
 var radius = 5;
 var rw = 20;
-var rh = 10;
-
-var svg = d3.select("body")
-    .append("svg")
-    .attr("width", 1000)
-    .attr("height", 1000);
-
-
-svg.append("defs").append("marker")
-    .attr("id", "arrowhead")
-    .attr("refX", radius * 2)
-    .attr("refY", radius)
-    .attr("markerWidth", 10)
-    .attr("markerHeight", 10)
-    .attr("orient", "auto")
-    .append("path")
-    .attr("d", "M 0,0 V 10 L 10,5 Z");
+var rh = 20;
 
 var diagonal = d3.svg.diagonal()
     .source(function (d) {
@@ -118,37 +115,167 @@ var diagonal = d3.svg.diagonal()
         return [d.y, d.x];
     });
 
-var link = svg.selectAll("path")
-    .data(links)
-    .enter().append("path")
-    .attr("class", "link")
-    .attr("d", diagonal)
-//    .attr("marker-end", "url(#arrowhead)");
-
-
-
-var rectangle = svg.selectAll("rect")
-    .data(rectList)
-    .enter().append("rect")
-    .attr("class", "rect")
-    .attr("x", function (d) {
-        return d.x - rw / 2;
-    })
-    .attr("y", function (d) {
-        return d.y - rh / 2;
-    })
-    .attr("width", rw)
-    .attr("height", rh);
-var circle = svg.selectAll("circle")
-    .data(circleList)
-    .enter().append("circle")
-    .attr("class", "circle")
-    .attr("cx", function (d) {
-        return d.x;
-    })
-    .attr("cy", function (d) {
-        return d.y;
-    })
-    .attr("r", function (d) {
-        return radius;
+function rect_mouseover(d) {
+    // bring to front
+    var d_id = "x" + d.x + "xy" + d.y + "y";
+    
+	d3.selectAll('.link').sort(function(a, b){ 
+        var s_id = "x" + a.source.x + "xy" + a.source.y + "y";
+        var t_id = "x" + a.target.x + "xy" + a.target.y + "y";
+        
+        return (d_id == s_id) || (d_id == t_id); 
     });
+    
+    //hightlight
+    var id = "x" + d.x + "xy" + d.y + "y";
+    d3.selectAll('#' + id).classed('rect-highlight', true);
+    d3.selectAll(".link[id*='" + id + "']").classed('link-highlight', true);
+    d3.selectAll('.link').each(function(d, i) {
+        var s_id = "x" + d.source.x + "xy" + d.source.y + "y";
+        var t_id = "x" + d.target.x + "xy" + d.target.y + "y";
+        
+        if (d_id == s_id) {
+            d3.selectAll(".link[id*='" + t_id + "']").classed('link-highlight', true);
+        }
+    });
+    
+    //show tooltips
+    showTooltips(getTooltips(d), d);
+}
+
+function rect_mouseout(d) {
+    var id = "x" + d.x + "xy" + d.y + "y";
+    var d_id = id;
+    d3.selectAll('#' + id).classed('rect-highlight', false);
+    d3.selectAll(".link[id*='" + id + "']").classed('link-highlight', false);
+    d3.selectAll('.link').each(function(d, i) {
+        var s_id = "x" + d.source.x + "xy" + d.source.y + "y";
+        var t_id = "x" + d.target.x + "xy" + d.target.y + "y";
+        
+        if (d_id == s_id) {
+            d3.selectAll(".link[id*='" + t_id + "']").classed('link-highlight', false);
+        }
+    });
+    
+    hideTooltips();
+}
+
+function circle_mouseover(d) {
+    // bring to front
+    var d_id = "x" + d.x + "xy" + d.y + "y";
+    
+	d3.selectAll('.link').sort(function(a, b){ 
+        var s_id = "x" + a.source.x + "xy" + a.source.y + "y";
+        var t_id = "x" + a.target.x + "xy" + a.target.y + "y";
+        
+        return (d_id == s_id) || (d_id == t_id); 
+    });
+    
+    //hightlight
+    var id = "x" + d.x + "xy" + d.y + "y";
+    d3.selectAll('#' + "x" + d.x + "y" + d.y).classed('circle-highlight', true);
+    d3.selectAll(".link[id*='" + id + "']").classed('link-highlight', true);
+    showTooltips(getTooltips(d), d);
+}
+
+function circle_mouseout(d) {
+    var id = "x" + d.x + "xy" + d.y + "y";
+    d3.selectAll('#' + id).classed('circle-highlight', false);
+    d3.selectAll(".link[id*='" + id + "']").classed('link-highlight', false);
+    hideTooltips();
+}
+
+var tooltip = d3.select(".tooltip");
+if(tooltip.empty()){    		 
+    tooltip = d3.select("body").append("div").attr("class", "tooltip");
+}
+tooltip.classed("hidden", true);
+
+function getTooltips(d) {
+    var tip = "";      			
+    tip += d.data.typeLabel + ": ";
+    tip += d.data.name;
+    tip += "<br/>Count: "; // This needs to be localized.
+    tip += d.data._weight;
+    
+    return tip;
+}
+
+function showTooltips(text, d) {
+    var id = "x" + d.x + "xy" + d.y + "y";
+    
+    var top = $("#" + id).offset().top;
+    var left = $("#" + id).offset().left + 10;
+    if (d.data.type == "event") {
+        top -= radius;
+        left += radius;
+    } else if (d.data.type == "source_target") {
+        top -= rh;
+        left += rw;
+    }
+    tooltip.classed("hidden", false);
+    tooltip.attr("style", "top:" + top + "px;"+"left:" + left + "px");
+    tooltip.html(text);
+}
+
+function hideTooltips() {
+    tooltip.classed("hidden", true);
+}
+
+function visualizeData(circleList, rectList, links) {
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", 1000)
+        .attr("height", 1000);
+
+    var link = svg.selectAll("path")
+        .data(links)
+        .enter().append("path")
+        .attr("class", "link")
+        .attr("id", function (d) {
+            return "s" +
+                "x" + d.source.x + "x" +
+                "y" + d.source.y + "y" +
+                "t" +
+                "x" + d.target.x + "x" +
+                "y" + d.target.y + "y";
+        })
+        .attr("d", diagonal);
+
+    var rectangle = svg.selectAll("rect")
+        .data(rectList)
+        .enter().append("rect")
+        .attr("class", "rect")
+        .attr('id', function (d) {
+            return "x" + d.x + "xy" + d.y + "y";
+        })
+        .attr("x", function (d) {
+            return d.x - rw / 2;
+        })
+        .attr("y", function (d) {
+            return d.y - rh / 2;
+        })
+        .attr("width", rw)
+        .attr("height", rh)
+        .on("mouseover", rect_mouseover)
+        .on("mouseout", rect_mouseout);
+
+    var circle = svg.selectAll("circle")
+        .data(circleList)
+        .enter().append("circle")
+        .attr("class", "circle")
+        .attr('id', function (d) {
+            return "x" + d.x + "xy" + d.y + "y";
+        })
+        .attr("cx", function (d) {
+            return d.x;
+        })
+        .attr("cy", function (d) {
+            return d.y;
+        })
+        .attr("r", function (d) {
+            return radius;
+        })
+        .on("mouseover", circle_mouseover)
+        .on("mouseout", circle_mouseout);
+}
