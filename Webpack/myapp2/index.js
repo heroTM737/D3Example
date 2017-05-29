c7fbb2dc77fc113f31e206daae8dd1f5bdbdfe8a
heroTM737 <xuantien.nguyen93@gmail.com>
2017-05-29 10:03:39 +0700

    update build

--- Webpack/myapp2/index.js
+++ Webpack/myapp2/index.js
@@ -1,4 +1,4 @@
-module.exports = {
+var data = {
     cities: [
         {
             name: "Paris",
@@ -46,4 +46,40 @@ module.exports = {
             latitude: 10.762622
         }
     ]
-};
+}
+
+$(document).ready(function () {
+    var refreshTime = 300;
+    var number_of_event = 3;
+    var cities = data.cities;
+
+    var genEvents = function () {
+        var events = [];
+        for (var i = 0; i < number_of_event; i++) {
+            var r1 = Math.floor(Math.random() * cities.length);
+            var r2 = null;
+            do {
+                r2 = Math.floor(Math.random() * cities.length);
+            } while (r2 == r1)
+
+            events.push({
+                source: cities[r1],
+                target: cities[r2]
+            });
+        }
+
+        return events;
+    }
+
+
+    var threatMap = window.createThreatMap(document.getElementsByClassName("mapcontainer")[0], genEvents(), 500, 500);
+
+    var autoRefresh = function() {
+        setTimeout(function(){
+            threatMap.update(genEvents());
+            autoRefresh();
+        }, refreshTime)
+    }
+    autoRefresh();
+
+});
--- Webpack/myapp2/webpack.config.js
+++ Webpack/myapp2/webpack.config.js
@@ -1,9 +1,9 @@
 var path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/threatmap.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'threatmap.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
