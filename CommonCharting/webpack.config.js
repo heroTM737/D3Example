c485e053e8043a2b6b72adb64d6bcf17bebac966
heroTM737 <xuantien.nguyen93@gmail.com>
2017-11-23 11:00:10 +0700

    update compile name

--- CommonCharting/webpack.config.js
+++ CommonCharting/webpack.config.js
@@ -3,8 +3,8 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: {
-    "drawGeoMap": './src/index.js',
-    "drawGeoMap.min": './src/index.js'
+    "CommonCharting": './src/index.js',
+    "CommonCharting.min": './src/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
